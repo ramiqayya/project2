0a6835fb7bffd02aa27ec5a85fda7d5ef7654f44
ramiqayya <rami.qayya@gmail.com>
2023-02-06 17:18:10 +0200

    modify add listing form page styles

--- commerce/auctions/forms.py
+++ commerce/auctions/forms.py
@@ -2,11 +2,16 @@ from django import forms
 
 
 class CreateListing(forms.Form):
-    title = forms.CharField(label="name", max_length=64)
-    description = forms.CharField(max_length=500)
-    starting_bid = forms.IntegerField(min_value=0)
-    image = forms.URLField()
-    category = forms.CharField(max_length=64)
+    title = forms.CharField(label="name", max_length=64, widget=forms.TextInput(
+        attrs={'class': 'form-control'}))
+    description = forms.CharField(
+        max_length=500, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    starting_bid = forms.IntegerField(
+        min_value=0, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    image = forms.URLField(widget=forms.TextInput(
+        attrs={'class': 'form-control'}))
+    category = forms.CharField(
+        max_length=64, widget=forms.TextInput(attrs={'class': 'form-control'}))
 
 
 class BidListing(forms.Form):
