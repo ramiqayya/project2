74909e21b7266b100ec211911cbfa9777d1e3344
ramiqayya <rami.qayya@gmail.com>
2023-02-06 18:14:45 +0200

    modifications on register, login and view_listing styles

--- commerce/auctions/forms.py
+++ commerce/auctions/forms.py
@@ -3,15 +3,15 @@ from django import forms
 
 class CreateListing(forms.Form):
     title = forms.CharField(label="name", max_length=64, widget=forms.TextInput(
-        attrs={'class': 'form-control'}))
-    description = forms.CharField(
-        max_length=500, widget=forms.TextInput(attrs={'class': 'form-control'}))
+        attrs={'class': 'form-control mb-2'}))
     starting_bid = forms.IntegerField(
-        min_value=0, widget=forms.TextInput(attrs={'class': 'form-control'}))
+        min_value=0, widget=forms.TextInput(attrs={'class': 'form-control mb-2'}))
     image = forms.URLField(widget=forms.TextInput(
         attrs={'class': 'form-control'}))
     category = forms.CharField(
-        max_length=64, widget=forms.TextInput(attrs={'class': 'form-control'}))
+        max_length=64, widget=forms.TextInput(attrs={'class': 'form-control mb-2'}))
+    description = forms.CharField(
+        max_length=500, widget=forms.Textarea(attrs={'rows': '5', 'class': 'form-control mb-2'}))
 
 
 class BidListing(forms.Form):
